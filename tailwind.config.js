af3a04d8f620196d2b7f1999f75ed438cf6162e3
priyamdavra <priyamdavra@gmail.com>
2025-01-23 20:22:24 +0530

    "add design for better look"

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,13 @@ export default {
       colors:{
         primary:"#06b6d4",
         secondary:"EF863E"
+      },
+
+      backgroundImage:{
+        'auth-bg--img': "url('./src/assets/images/auth-img.png')",
+        'profile-bg--img':"url('./src/assets/images/profile-bg.png')",
       }
+
     },
   },
   plugins: [],
